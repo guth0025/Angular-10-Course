2c1deb844b84f79877e02cde0ee49a2895bfd796
guth0025 <guth0025@algonquinlive.com>
2021-03-11 11:06:45 -0500

    Lab 8

---
+++ src/app/vehicle-form-reactive/vehicle-form-reactive.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VehicleFormReactiveComponent } from './vehicle-form-reactive.component';
+
+describe('VehicleFormReactiveComponent', () => {
+  let component: VehicleFormReactiveComponent;
+  let fixture: ComponentFixture<VehicleFormReactiveComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VehicleFormReactiveComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VehicleFormReactiveComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/vehicle-form-reactive/vehicle-form-reactive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-form-reactive',
+  templateUrl: './vehicle-form-reactive.component.html',
+  styleUrls: ['./vehicle-form-reactive.component.css']
+})
+export class VehicleFormReactiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
